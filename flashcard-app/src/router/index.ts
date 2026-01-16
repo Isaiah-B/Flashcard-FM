879e6fee3a19b07d45f58186945b39b045279b87
Isaiah-B <isaiahbell333@gmail.com>
2026-01-16 15:50:07 -0600

    Initial commit

---
+++ flashcard-app/src/router/index.ts
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import StudyModeView from '@/views/StudyModeView.vue'
+import AllCardsView from '@/views/AllCardsView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'study',
+      component: StudyModeView
+    },
+    {
+      path: '/cards',
+      name: 'cards',
+      component: () => import('../views/AllCardsView.vue'),
+    }
+  ]
+  // routes: [
+  //   {
+  //     path: '/',
+  //     name: 'home',
+  //     component: HomeView,
+  //   },
+  //   {
+  //     path: '/about',
+  //     name: 'about',
+  //     // route level code-splitting
+  //     // this generates a separate chunk (About.[hash].js) for this route
+  //     // which is lazy-loaded when the route is visited.
+  //     component: () => import('../views/AboutView.vue'),
+  //   },
+  // ],
+})
+
+export default router
